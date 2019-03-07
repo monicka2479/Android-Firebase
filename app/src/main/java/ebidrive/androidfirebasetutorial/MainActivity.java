38a3e03d97e2ef7743e29c7afad322829321b6d3
Monicka Ruthrakumaran <monicka.rk@slenix.com>
2019-03-07 22:41:59 +0530

    Add Files

---
+++ app/src/main/java/ebidrive/androidfirebasetutorial/MainActivity.java
@@ -0,0 +1,28 @@
+package ebidrive.androidfirebasetutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Add Toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Aranidhi");
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
